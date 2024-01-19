c9de88646fb3459e4b7d45ca425a8ff3f76d9170
Avi Deitcher <avi@deitcher.net>
2024-01-19 14:41:11 +0200

    include sysctl settings
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- install.sh
+++ install.sh
@@ -289,6 +289,17 @@ else
   ${funcname} ${osfull}" "${arch}" "${version}"
 fi
 
+# set system settings
+echo br_netfilter > /etc/modules-load.d/br_netfilter.conf
+systemctl restart systemd-modules-load.service
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward=1
+EOF
+sysctl --system --quiet
+
+
 crisock=""
 case $runtime in
   "docker")
